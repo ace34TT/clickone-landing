6997bec090b916f77f52dea4cd664b29fd255453
aceky <tafinasoa35@gmail.com>
2026-04-01 12:09:30 +0300

    feat: integrate Lenis smooth scrolling provider and update global styles

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 import { HeaderThemeProvider } from './HeaderTheme'
 import { ThemeProvider } from './Theme'
+import { LenisProvider } from './LenisProvider'
 
 export const Providers: React.FC<{
   children: React.ReactNode
 }> = ({ children }) => {
   return (
-    <ThemeProvider>
-      <HeaderThemeProvider>{children}</HeaderThemeProvider>
-    </ThemeProvider>
+    <LenisProvider>
+      <ThemeProvider>
+        <HeaderThemeProvider>{children}</HeaderThemeProvider>
+      </ThemeProvider>
+    </LenisProvider>
   )
 }
